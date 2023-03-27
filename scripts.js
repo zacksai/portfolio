e9eb8cfac08dee810da1d51bf86746cc522634d3
Zack Sai <saidane@usc.edu>
2023-03-26 18:30:44 -0700

    fixed spacing of background & nav

--- scripts.js
+++ scripts.js
@@ -3,36 +3,41 @@ const toggleNav = () => {
     document.body.dataset.nav = document.body.dataset.nav === "true" ? "false" : "true";
 }
 
+const toggleButtonClicked = () => {
+    document.body.dataset.buttonClicked = document.body.dataset.buttonClicked === "true" ? "false" : "true";
+}
+
+
 // get img-track element
 const track = document.getElementById("image-track");
 
-// When mouse button is pressed or touch starts, store the X position
+// Store X on press
 const handleOnDown = e => track.dataset.mouseDownAt = e.clientX;
 
-// When mouse button is released or touch ends, reset mouseDownAt and store the current percentage
+// Store percentage, reset mouse down
 const handleOnUp = () => {
     track.dataset.mouseDownAt = "0";
     track.dataset.prevPercentage = track.dataset.percentage;
 }
 
-// When the mouse is moved or touch moves, calculate the new percentage and apply the transformation
+// Calculate % based off movement and transform
 const handleOnMove = e => {
-    // If mouseDownAt is "0", do nothing
+    // Do nothing at 0
     if (track.dataset.mouseDownAt === "0") return;
 
-    // Calculate the difference between the starting X position and the current X position
+    // Find difference
     const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
         maxDelta = window.innerWidth / 2;
 
-    // Calculate the percentage of movement based on maxDelta
+    // Calculate %
     const percentage = (mouseDelta / maxDelta) * -100,
         nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage,
         nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
 
-    // Store the new percentage in the dataset
+    // Store %
     track.dataset.percentage = nextPercentage;
 
-    // Animate the track element's transform property
+    // Animate transformation
     track.animate({
         transform: `translate(${nextPercentage}%, -50%)`
     }, {duration: 1200, fill: "forwards"});
@@ -44,16 +49,10 @@ const handleOnMove = e => {
     }
 }
 
-// This code sets up event listeners for mouse and touch interactions and passes them to the relevant functions.
-
-window.onmousedown = e => handleOnDown(e); // Listens for the mouse button to be pressed down and passes the event object to handleOnDown function.
-
-window.ontouchstart = e => handleOnDown(e.touches[0]); // Listens for a touch to be initiated and passes the first touch point to handleOnDown function.
-
-window.onmouseup = e => handleOnUp(e); // Listens for the mouse button to be released and passes the event object to handleOnUp function.
-
-window.ontouchend = e => handleOnUp(e.touches[0]); // Listens for the touch to end and passes the first touch point to handleOnUp function.
-
-window.onmousemove = e => handleOnMove(e); // Listens for the mouse to move and passes the event object to handleOnMove function.
-
-window.ontouchmove = e => handleOnMove(e.touches[0]); // Listens for a touch to move and passes the first touch point to handleOnMove function.
+// listeners
+window.onmousedown = e => handleOnDown(e);
+window.ontouchstart = e => handleOnDown(e.touches[0]);
+window.onmouseup = e => handleOnUp(e);
+window.ontouchend = e => handleOnUp(e.touches[0]);
+window.onmousemove = e => handleOnMove(e);
+window.ontouchmove = e => handleOnMove(e.touches[0]);
