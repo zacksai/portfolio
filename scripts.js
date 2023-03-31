c67288039086672b9032a08f1bbc34e5773b42f9
Zack Sai <saidane@usc.edu>
2023-03-31 04:31:43 -0700

    organized code and added documentation. complete... for now

--- scripts.js
+++ scripts.js
@@ -4,17 +4,18 @@ window.onload = function () {
     animateNavToggle();
 }
 
-// mouse over toggle listeners
+// Opening hover
 let isMouseOverNavToggle = false;
 let firstHover = false;
 const navToggle = document.getElementById("nav-toggle");
 const openIcon = document.querySelector(".openUp");
 
-// Add event listeners to detect hover state
+// Mouse enter toggle listener
 navToggle.addEventListener("mouseenter", () => {
     isMouseOverNavToggle = true;
     firstHover = true;
 });
+// Mouse exit toggle listener
 navToggle.addEventListener("mouseleave", () => {
     isMouseOverNavToggle = false;
     // reset when mouse leaves nav toggle after it has entered on prompt
@@ -25,7 +26,7 @@ navToggle.addEventListener("mouseleave", () => {
 // Prompt nav-toggle after a bit
 function animateNavToggle() {
 
-    // prompt after a bit
+    // Hover after a moment
     setTimeout(() => {
 
         // only execute if first hover hasn't happened
@@ -42,13 +43,12 @@ function animateNavToggle() {
             // add another event listener to apply the hover styling again
             navToggle.addEventListener("mouseenter", () => {
                 navToggle.style.transform = "scale(1.17)";
-                if (document.body.dataset.state == "S1" || document.body.dataset.state == "S4") {
+                if (document.body.dataset.state === "S1" || document.body.dataset.state === "S4") {
                     openIcon.style.opacity = "1";
 
                 }
             });
-        }, 1500);
-
+        }, 3000);
     }, 100);
 
 }
@@ -60,17 +60,17 @@ const toggleNav = () => {
     document.body.dataset.nav = document.body.dataset.nav === "true" ? "false" : "true";
 
     // move to appropriate state, read "when toggle is clicked in each state, go to this state"
-    if (document.body.dataset.state == "S1") {
+    if (document.body.dataset.state === "S1") {
         // update open icon if it was set to 1 from prompt
-        if (openIcon.style.opacity == "1") {
+        if (openIcon.style.opacity === "1") {
             openIcon.style.opacity = "0";
         }
         document.body.dataset.state = "S2";
-    } else if (document.body.dataset.state == "S2") {
+    } else if (document.body.dataset.state === "S2") {
         document.body.dataset.state = "S1";
-    } else if (document.body.dataset.state == "S3") {
+    } else if (document.body.dataset.state === "S3") {
         document.body.dataset.state = "S4";
-    } else if (document.body.dataset.state == "S4") {
+    } else if (document.body.dataset.state === "S4") {
         document.body.dataset.state = "S3";
     }
 }
@@ -112,126 +112,102 @@ const toggleButtonClicked = (buttonType) => {
             selectedDisplays.style.display = 'flex';
             track = selectedDisplays; // Update the 'track' variable
         }
-    }, 1100); // Delay the execution of the code for 1100ms to allow the previous track to finish its animation up
+    }, 1100); // Make visible after prev animation finished
 
 
     // toggle when clicked (if it's true, set it to false, else set it to true)
     document.body.dataset.buttonClicked = document.body.dataset.buttonClicked === "true" ? "false" : "true";
 
     // move to appropriate state, read "when button is clicked in each state, go to this state"
-    if (document.body.dataset.state == "S2") { // "immerse"
+    if (document.body.dataset.state === "S2") { // "immerse"
         // move to state 3, reset button to true (both nav & bc should be true)
         document.body.dataset.state = "S3";
         document.body.dataset.buttonClicked = "true";
-    } else if (document.body.dataset.state == "S4") {
+    } else if (document.body.dataset.state === "S4") {
         document.body.dataset.state = "S3";
         document.body.dataset.nav = "true";
         document.body.dataset.buttonClicked = "true";
     }
 }
 
-const filmReel = document.getElementById('film');
-const projectsReel = document.getElementById('projects');
+// store scrollable displays
+const nodeList = [
+    document.getElementById('film'),
+    document.getElementById('work')
+];
 
 // Store X on press
 const handleOnTouchDown = (e) => {
-    filmReel.dataset.mouseDownAt = e.clientX
-    projectsReel.dataset.mouseDownAt = e.clientX
+    for (const reel of nodeList) {
+        reel.dataset.mouseDownAt = e.clientX;
+    }
 };
 
 // Store percentage, reset mouse down
 const handleOnTouchUp = () => {
-    filmReel.dataset.mouseDownAt = "0";
-    filmReel.dataset.prevPercentage = filmReel.dataset.percentage;
-    projectsReel.dataset.mouseDownAt = "0";
-    projectsReel.dataset.prevPercentage = filmReel.dataset.percentage;
+    for (const reel of nodeList) {
+        reel.dataset.mouseDownAt = "0";
+        reel.dataset.prevPercentage = reel.dataset.percentage;
+    }
 };
 
 // Calculate % based off movement and transform
 const handleOnTouchMove = (e) => {
-    // Do nothing at 0
-    if (filmReel.dataset.mouseDownAt === "0") return;
+    for (const reel of nodeList) {
 
-    // Find difference
-    let mouseDelta = parseFloat(filmReel.dataset.mouseDownAt) - e.clientX,
-        maxDelta = window.innerWidth / 2;
+        // Do nothing at 0
+        if (reel.dataset.mouseDownAt === "0") return;
 
-    // Calculate %
-    let percentage = (mouseDelta / maxDelta) * -100,
-        nextPercentageUnconstrained =
-            parseFloat(track.dataset.prevPercentage) + percentage,
-        nextPercentage = Math.max(
-            Math.min(nextPercentageUnconstrained, 0),
-            -100
-        );
+        // Find difference
+        let mouseDelta = parseFloat(reel.dataset.mouseDownAt) - e.clientX,
+            maxDelta = window.innerWidth / 2;
 
-    // Store %
-    filmReel.dataset.percentage = nextPercentage;
+        // Calculate %
+        let percentage = (mouseDelta / maxDelta) * -100,
+            nextPercentageUnconstrained =
+                parseFloat(track.dataset.prevPercentage) + percentage,
+            nextPercentage = Math.max(
+                Math.min(nextPercentageUnconstrained, 0),
+                -100
+            );
 
-    // Animate transformation differently on mobile and desktop (smooth)
-    if (!(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent))) {
-        filmReel.animate(
-            {
-                transform: `translate(${nextPercentage}%, 0%)`,
-            },
-            {duration: 1200, fill: "forwards"}
-        );
-        for (const image of filmReel.getElementsByClassName("filmImage")) {
-            image.animate(
+        // Store %
+        reel.dataset.percentage = nextPercentage;
+
+        // Animate transformation differently on mobile and desktop (smooth)
+        if (!(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent))) {
+            reel.animate(
                 {
-                    objectPosition: `${100 + nextPercentage}% center`,
+                    transform: `translate(${nextPercentage}%, 0%)`,
                 },
                 {duration: 1200, fill: "forwards"}
             );
-        }
-    } else {
-        // Animate transformation
-        const animate = () => {
-            filmReel.style.transform = `translate(${nextPercentage}%, 0%)`;
-            for (const image of filmReel.getElementsByClassName("filmImage")) {
-                image.style.objectPosition = `${100 + nextPercentage}% center`;
+
+            // animate object position if film
+            if (reel.id === 'film') {
+                for (const image of reel.getElementsByClassName("filmImage")) {
+                    image.animate(
+                        {
+                            objectPosition: `${100 + nextPercentage}% center`,
+                        },
+                        {duration: 1200, fill: "forwards"}
+                    );
+                }
             }
-        };
-        requestAnimationFrame(animate);
+        } else {
+            // Animate transformation
+            const animate = () => {
+                reel.style.transform = `translate(${nextPercentage}%, 0%)`;
+                // animate object position if film
+                if (reel.id === 'film') {
+                    for (const image of reel.getElementsByClassName("filmImage")) {
+                        image.style.objectPosition = `${100 + nextPercentage}% center`;
+                    }
+                }
+            };
+            requestAnimationFrame(animate);
+        }
     }
-
-    // repeat for projects reel
-    // Do nothing at 0
-    if (projectsReel.dataset.mouseDownAt === "0") return;
-
-    // Find difference
-    mouseDelta = parseFloat(projectsReel.dataset.mouseDownAt) - e.clientX;
-    maxDelta = window.innerWidth / 2;
-
-    // Calculate %
-    percentage = (mouseDelta / maxDelta) * -100;
-    nextPercentageUnconstrained =
-        parseFloat(track.dataset.prevPercentage) + percentage,
-        nextPercentage = Math.max(
-            Math.min(nextPercentageUnconstrained, 0),
-            -100
-        );
-
-    // Store %
-    projectsReel.dataset.percentage = nextPercentage;
-
-    // Animate transformation differently on mobile and desktop (smooth)
-    if (!(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent))) {
-        projectsReel.animate(
-            {
-                transform: `translate(${nextPercentage}%, 0%)`,
-            },
-            {duration: 1200, fill: "forwards"}
-        );
-    } else {
-        // Animate transformation
-        const animate = () => {
-            projectsReel.style.transform = `translate(${nextPercentage}%, 0%)`;
-
-        };
-        requestAnimationFrame(animate);
-    }
-
 };
 
 // listeners
